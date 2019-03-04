2aefc91735dde64d8b9c892c94d7f19b6ff6ccf3
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-04 16:53:18 -0500

    Change logo to link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 
 import logo from '../../assets/images/logo.jpg';
 
-const Logo = styled.img`
+const Logo = styled.figure`
   grid-column-end: span 6;
   padding: 10px;
+  margin: 0;
 `;
 
 const LogInLink = styled.div`
@@ -21,10 +22,11 @@ const LogInLink = styled.div`
 
 const Header = () => (
   <>
-    <Logo
-      src={logo}
-      width="110px"
-    />
+    <Logo>
+      <Link to="/">
+        <img src={logo} alt="logo" width="100px" />
+      </Link>
+    </Logo>
 
     <LogInLink>
       <Link to="login">LOG IN</Link>
