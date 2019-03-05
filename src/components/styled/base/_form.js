6c609232006318ab43822c3fd13c179b95b841e7
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-05 04:42:46 -0500

    Create styled form components

---
+++ src/components/styled/base/_form.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { tundora, codGray } from './_colors';
+
+export const Input = styled.input`
+  height: 38px;
+  padding: 6px 10px; /* The 6px vertically centers text on FF, ignored by Webkit */
+  background-color: white;
+  border: 1px solid ${tundora};
+  border-radius: 4px;
+  box-shadow: none;
+  box-sizing: border-box;
+  appearance: none;
+  
+  &:focus {
+    border: 1px solid ${codGray};
+    outline: 0;
+  }
+`;
+
+export const Textarea = styled.textarea`
+  padding: 6px 10px; 
+  background-color: #fff;
+  border: 1px solid ${tundora};
+  border-radius: 4px;
+  box-shadow: none;
+  box-sizing: border-box;
+  appearance: none;
+  min-height: 65px;
+  /* padding-top: 6px; */
+  /* padding-bottom: 6px; */
+  
+  &:focus {
+    border: 1px solid ${codGray};
+    outline: 0;
+  }
+`;
+
+export const Label = styled.label`
+  display: block;
+  margin-bottom: .5rem;
+  font-weight: 600;
+`;
